e09d084f5945b9a988866dfbc55441cfe796495b
Cássia <cassiarsqueiroz@gmail.com>
2022-11-15 20:28:21 -0300

    Modify Java example

--- src/main/java/io/github/cassiaqueiroz/JavaExample.java
+++ src/main/java/io/github/cassiaqueiroz/JavaExample.java
@@ -4,7 +4,7 @@ public class JavaExample {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, World! Hola, mundo!");
+        System.out.println("Hello, World!");
 
     }
 }
