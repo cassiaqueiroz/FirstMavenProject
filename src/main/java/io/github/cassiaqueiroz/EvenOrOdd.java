8659865e3846f82fe39e8f6086598013ae334bcf
Cássia <cassiarsqueiroz@gmail.com>
2022-12-14 02:53:14 -0300

    EvenOrOdd class creation

---
+++ src/main/java/io/github/cassiaqueiroz/EvenOrOdd.java
@@ -0,0 +1,18 @@
+package io.github.cassiaqueiroz;
+
+public class EvenOrOdd {
+
+    public static void main(String[ ] args) {
+
+            int evenOdd = 73;
+
+            if (evenOdd %2 ==1) { //to figure out even numbers: “%2 == 0”
+
+                System.out.print("Este número es impar.");
+            }else {
+                System.out.print("Este número es par.");
+
+            }
+
+    }
+}
