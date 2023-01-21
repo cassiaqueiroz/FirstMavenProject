a4c4b3978829fbef760873b9b0c999c8ba9177f2
Cássia <cassiarsqueiroz@gmail.com>
2023-01-21 20:00:03 -0300

    Correcting variable declaration and Reformat OneToTen

--- src/main/java/io/github/cassiaqueiroz/OneToTen.java
+++ src/main/java/io/github/cassiaqueiroz/OneToTen.java
@@ -4,20 +4,18 @@ public class OneToTen {
 
     public static void main(String[] args) {
 
-            int number;
+        int number = 1; // Declaration and Initialization
 
-            number = 1;
+        while (number <= 10) {
 
-            while (number <= 10) {
+            System.out.println(number);
 
-                System.out.println(number);
-
-                if(number <= 10) {
-                    number++;
-
-                }
+            if (number <= 10) {
+                number++;
 
             }
+
+        }
 
     }
 
